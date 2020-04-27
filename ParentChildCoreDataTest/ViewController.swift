c7b709816dabcc0f3196bf2c7bc104ef3d11493b
Gernot Poetsch <github@poetsch.org>
2020-04-27 17:24:12 +0200

    Initial Commit

---
+++ ParentChildCoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParentChildCoreDataTest
+//
+//  Created by Gernot Poetsch on 27.04.20.
+//  Copyright © 2020 nxtbgthng GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
